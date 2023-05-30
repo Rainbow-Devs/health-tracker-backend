7381add9f5eb936242ac536f11a32ed555b4df20
Spencer W <4006420+seidior@users.noreply.github.com>
2023-05-30 16:12:53 -0700

    #6 Add sundry items to project

--- healthtracker/schema.py
+++ healthtracker/schema.py
@@ -1,6 +1,19 @@
+"""
+Django endpoint for GraphQL via Graphene.
+
+Learn more at https://docs.graphene-python.org/en/latest/quickstart
+"""
+
 import graphene
 
+
 class Query(graphene.ObjectType):
+    """
+    Class that handles GraphQL queries
+    """
+
     hello = graphene.String(default_value="Hi!")
+    ping = graphene.String(default_value="pong")
+
 
 schema = graphene.Schema(query=Query)
