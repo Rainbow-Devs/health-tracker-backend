f4b6d30046e3f718a8d99b691badb7fafc58b43d
Aidan Margo <aidanmargo@gmail.com>
2023-06-27 09:27:05 -0500

    #14 Added comments to models

--- base_login/models.py
+++ base_login/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
 
+#Activity Categories
 class ActivityTypes(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,6 +10,7 @@ class ActivityTypes(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+# User activities
 class Activities(models.Model):
     name = models.CharField(max_length=255)
     type = models.ForeignKey('ActivityTypes', on_delete=models.CASCADE)
